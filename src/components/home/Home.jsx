405a2a037555db38589108bb6a92881b8e7d9e46
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2019-01-28 18:08:29 -0600

    Home Carousel component changed to react-fully-scrolled

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,37 +1,35 @@
 import React, { Component } from 'react'
-import './Home.css'
-import { Carousel } from 'antd';
-import HomeLanding from './HomeLanding'
-import HomeVideo from './HomeVideo'
-import HomeObjetivos from './HomeObjetivos'
+import { Scroller, Section } from 'react-fully-scrolled';
+import HomeLanding from './HomeLanding';
+import HomeVideo from './HomeVideo';
+import HomeObjetivos from './HomeObjetivos';
+
 
 export default class Home extends Component {
-  myRef = React.createRef()
-  state = {
-    scrollTop: 0
-  }
-
   onClick = e => {
     // toggles play / pause
     e.target.paused ? e.target.play() : e.target.pause()
   }
 
-  onObjClick = e => {
-    console.log(e.target)
-  }
-
-  
-
   render() {
     const {onClick} = this
     return (
-      <div className="home-carousel">
-        <Carousel vertical autoplay>
+      <Scroller
+        curPage={1}
+        onBeforeScroll={(from, to) => {}}
+        onAfterScroll={(page) => {}}
+        isEnabled={true}
+      >
+        <Section>
           <HomeLanding />
+        </Section>
+        <Section>
           <HomeVideo onClick={onClick} />
+        </Section>
+        <Section>
           <HomeObjetivos />
-        </Carousel>
-      </div>
+        </Section>
+      </Scroller>
     )
   }
 }
